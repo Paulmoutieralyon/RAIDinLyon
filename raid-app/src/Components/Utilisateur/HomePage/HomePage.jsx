ad235c47725128a1ec35b64ebfa021071e9e321e
Paul Moutier <moutier.paul@gmail.com>
2018-11-21 13:54:20 +0100

    Home Page butoon logo info

--- raid-app/src/Components/Utilisateur/HomePage/HomePage.jsx
+++ raid-app/src/Components/Utilisateur/HomePage/HomePage.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import '../../../App.css';
+import { NavLink } from 'react-router-dom';
+import logo from '../../../Img/RaidLyonLogo.png';
+import info from '../../../Img/info.png'
 
 export default class HomePage extends React.Component {
     constructor(props) {
@@ -8,7 +12,12 @@ export default class HomePage extends React.Component {
 
     render() {
         return (
-            <div> hello ici la page </div>
+            <div>
+                <img className="LogoImg" src={logo} alt='homelogo' />
+                <img className="infologo" src={info} alt='homeinfo' />
+                <h1 className="TitreSession"> BIENVENUE <br /> A <br />RaidInLyon</h1>
+                <NavLink to="../../MapEgnime/MapEgnime"><button className="Button1">Lancer la Partie</button></NavLink>
+            </div>
         );
     }
 }
