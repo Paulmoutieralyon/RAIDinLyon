7f32e23ca5a60a9e159478ba77acd21062b978b9
Paul Moutier <moutier.paul@gmail.com>
2018-11-28 14:00:16 +0100

    Bouton infos

--- raid-app/src/Components/Utilisateur/EnigmePage/EnigmePage.jsx
+++ raid-app/src/Components/Utilisateur/EnigmePage/EnigmePage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { AvForm, AvField } from 'availity-reactstrap-validation';
-import { Button } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+import { Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import './EnigmePage.css';
 import info from './info.1.png';
 import Pierrephilosophale from './Pierrephilosophale.jpeg';
 import Faux from './faux.png';
 import Vrai from './vrai.png';
 import Vide from './Vide.png';
+import './InfosModalEgnime.css';
 
 export default class EnigmePage extends React.Component {
     constructor(props) {
@@ -16,9 +17,15 @@ export default class EnigmePage extends React.Component {
             reponse: ["la pierre philosophale", "pierre philosophale"],
             proposition: "",
             final: Vide,
+            modal: false
         };
+        this.toggle = this.toggle.bind(this);
     }
-
+    toggle() {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
     isProposing = (e) => {
         this.setState({
             proposition: e.target.value
@@ -46,7 +53,12 @@ export default class EnigmePage extends React.Component {
 
             <div>
                 <NavLink to="/MapPage"><button className="ButtonBack"> Retour </button></NavLink>
-                <img className="infologo" src={info} alt='egnimeInfo' />
+                <img className='Infologo'  onClick={this.toggle}  src={info} alt='infologo'>{this.props.buttonLabel}</img>
+                        <Modal className='Modale' isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                            <ModalHeader toggle={this.toggle}>Petites règles dans ce lieu </ModalHeader>
+                            <ModalBody className='modaltexte'>
+                       Ne parlez pas trop fort ! les murs sont sensibles ^^     </ModalBody>
+                        </Modal>
 
                 <img className="Illustration" src={Pierrephilosophale} />
                 <p className="Titre">Nicolas Flamel </p>
@@ -55,7 +67,7 @@ export default class EnigmePage extends React.Component {
                 <AvForm className="reponse" onSubmit={this.isTrue}>
                     <h3 className="TitreQuestion">Quelle découverte a rendu célèbre Nicolas Flamel ?</h3>
                     <AvField name="enigme" type="text" placeholder="votre réponse" onChange={this.isProposing} />
-                    <Button color="primary">Validé</Button>
+                    <Button color="primary">Valider</Button>
                     <img className="final" src={this.state.final} />
                 </AvForm>
 
