85bc0921da8a8f0f4cea3ab23ef53b69778a9a49
alexcanguilhem <alexandre.canguilhem@gmail.com>
2018-11-29 11:32:12 +0100

    changement indices

--- raid-app/src/Components/Utilisateur/EnigmePage/EnigmePage.jsx
+++ raid-app/src/Components/Utilisateur/EnigmePage/EnigmePage.jsx
@@ -16,8 +16,23 @@ export default class EnigmePage extends React.Component {
             reponse: ["la pierre philosophale", "pierre philosophale"],
             proposition: "",
             final: Vide,
+            indice: null,
+            indiceNumber: 0,
         };
     }
+
+    indices = () => {
+        this.setState({ indiceNumber: this.state.indiceNumber + 1 })
+        if (this.state.indiceNumber === 0) {
+            this.setState({ indice: "Harry Potter" })
+        }
+        if (this.state.indiceNumber === 1) {
+            this.setState({ indice: "Pernelle" })
+        }
+        if (this.state.indiceNumber === 2) {
+            this.setState({ indice: "Amis de Albus, et Francais'" })
+        }
+    };
 
     isProposing = (e) => {
         this.setState({
@@ -46,9 +61,9 @@ export default class EnigmePage extends React.Component {
 
             <div>
                 <NavLink to="/MapPage"><button className="ButtonBack"> Retour </button></NavLink>
-                <img className="bontonInfo" src={Info} />
+                <img className="bontonInfo" src={Info} alt="" />
 
-                <img className="Illustration" src={Pierrephilosophale} />
+                <img className="Illustration" src={Pierrephilosophale} alt=""/>
                 <p className="Titre">Nicolas Flamel </p>
                 <p>Nicolas Flamel, éminent personnage du XIVème siècle est essentiellement réputé comme étant l’alchimiste ayant réussi dans la quête de la Pierre Philosophale. On attribuait à cette pierre certaines propriétés dont celle de pouvoir transmuter les métaux vils en métaux précieux comme l’or ou l’argent.</p>
 
@@ -56,7 +71,9 @@ export default class EnigmePage extends React.Component {
                     <h3>Quelle découverte a rendu célèbre Nicolas Flamel ?</h3>
                     <AvField name="enigme" type="text" onChange={this.isProposing} />
                     <Button color="primary">Submit</Button>
-                    <img className="final" src={this.state.final} />
+                    <img className="final" src={this.state.final} alt="" />
+                    <Button onClick={this.indices} classname="bonton2" >Indice</Button>
+                    {this.state.indice}
                 </AvForm>
 
             </div>
