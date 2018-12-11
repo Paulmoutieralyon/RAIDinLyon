c63c01508caf594c770b53c00bb840c6b52b6b2c
Simon <Simon-Vancoillie_student2018@wilder.school>
2018-12-11 17:35:29 +0100

    reabilitation prison

--- raid-app/src/Reducers/allReducers.jsx
+++ raid-app/src/Reducers/allReducers.jsx
@@ -3,6 +3,8 @@ import reducerEnigmePage from './Utilisateur/EnigmePage/reducerEnigmePage'
 import reducerHomePage from './Utilisateur/HomePage/reducerHomePage'
 import pointManagement from './Utilisateur/HomePage/pointManagement'
 import reducerMapPage from './Utilisateur/MapPage/reducerMapPage'
+import titleManagement from './Utilisateur/MapPage/titleManagement'
+
 
 
 const allReducers = combineReducers({
@@ -10,7 +12,7 @@ const allReducers = combineReducers({
     reducerHomePage: reducerHomePage,
     pointManagement: pointManagement,
     reducerMapPage: reducerMapPage,
-    
+    titleManagement: titleManagement,
 })
 
 export default allReducers
