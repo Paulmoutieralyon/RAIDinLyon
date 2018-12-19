780a92259668e47069ada11a234ded15df133065
alex canguilhem <alexandre.canguilhem@gmail.com>
2018-12-19 09:18:47 +0100

    buttonv3

--- raid-app/src/Components/Utilisateur/EnigmePage/EnigmePage.jsx
+++ raid-app/src/Components/Utilisateur/EnigmePage/EnigmePage.jsx
@@ -18,6 +18,7 @@ export class EnigmePage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            compteurcontinue: 0,
             proposition: "",
             final: Vide,
             modal: false,
@@ -37,10 +38,11 @@ export class EnigmePage extends React.Component {
             img: "./Pierrephilosophale.jpeg",
         };
         this.toggle = this.toggle.bind(this);
+        this.data = null
     }
 
     componentDidMount = () => {
-        fetch("http://localhost:5000/api/enigmes")
+        /* fetch("http://localhost:5000/api/enigmes")
             .then(laPetiteReponse => {
                 return laPetiteReponse.json()
             })
@@ -54,7 +56,30 @@ export class EnigmePage extends React.Component {
                     info: data[0].info,
                     img: data[0].img,
                 })
+            }) */
+        fetch("http://localhost:5000/api/enigmes")
+            .then(laPetiteReponse => {
+                return laPetiteReponse.json()
             })
+            .then(data => {
+                this.data = data
+
+                this.setState({
+                    question: data[2].question,
+                    titre: data[2].titre,
+                    texte: data[2].texte,
+                    reponse: data[2].reponse,
+                    indices: data[2].indices,
+                    info: data[2].info,
+                    img: data[2].img,
+                })
+
+                this.setState({
+                    isFloat: true
+                })
+            })
+
+
     }
 
     toggle() {
@@ -65,14 +90,16 @@ export class EnigmePage extends React.Component {
 
     indices = () => {
         this.setState({ indiceNumber: this.state.indiceNumber + 1 })
+
+
         if (this.state.indiceNumber === 0) {
-            this.setState({ indice: "Harry Potter" })
+            this.setState({ indice: this.data[2].indices[0] })
         }
         if (this.state.indiceNumber === 1) {
-            this.setState({ indice: "Pernelle" })
+            this.setState({ indice: this.data[2].indices[1] })
         }
         if (this.state.indiceNumber === 2) {
-            this.setState({ indice: "Amis de Albus" })
+            this.setState({ indice: this.data[2].indices[2] })
         }
     };
 
@@ -116,7 +143,7 @@ export class EnigmePage extends React.Component {
         return (
 
             <div class="EnigmePageContainer">
-                {/*<NavLink to="/MapPage"><button className="ButtonBack"> Retour </button></NavLink>*/}
+                <NavLink to="/MapPage"><button className="ButtonBack"> Retour </button></NavLink>
                 {/*<img className="bontonInfo" src={Info} alt="" />*/}
                 <img className='Infologoegnime' onClick={this.toggle} src={info} alt='infologo'>{this.props.buttonLabel}</img>
                 <Modal className='Modale' isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
