22a81da86664fea40ac74f0d31c01b27f396c821
Paul moutier <moutier.paul@gmail.com>
2019-01-04 15:19:56 +0100

    qrcode

--- raid-app/src/App.js
+++ raid-app/src/App.js
@@ -14,7 +14,7 @@ import ListSessionPage from './Components/Admin/ListSessionPage';
 import SessionPage from './Components/Admin/SessionPage';
 import Listequipes from './Components/Admin/ListEquipes';
 import Addsession from './Components/Admin/Addsession';
-
+import QrCodeScan from './Components/Admin/QrCodeScan';
 
 export default class App extends Component {
 
@@ -38,6 +38,7 @@ export default class App extends Component {
             <Route path="/Admin/SessionPage" component={SessionPage} />
             <Route path="/Admin/AddEgnimes" component={AddEgnimes} />
             <Route path='/Admin/Addsession' component={Addsession} />
+            <Route path='/Admin/QrCodeScan' component={QrCodeScan} />
           </Switch>
         </BrowserRouter>
 
