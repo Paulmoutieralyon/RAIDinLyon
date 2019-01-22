f36fbc13020fc8d3da3cad9a7f664e5ba6899a89
Paul moutier <moutier.paul@gmail.com>
2019-01-22 19:01:56 +0100

    required

--- raid-app/src/Components/Admin/AddEgnimes.jsx
+++ raid-app/src/Components/Admin/AddEgnimes.jsx
@@ -3,6 +3,21 @@ import { Button, Card, Input, Label, FormGroup, FormText } from 'reactstrap';
 import axios from 'axios'
 import { NavLink } from 'react-router-dom';
 
+const formValid = ({ formErrors, ...rest }) => {
+    let valid = true;
+
+    // validate form errors being empty
+    Object.values(formErrors).forEach(val => {
+        val.length > 0 && (valid = false);
+    });
+
+    // validate the form was filled out
+    Object.values(rest).forEach(val => {
+        val === null && (valid = false);
+    });
+
+    return valid;
+};
 
 export default class AddEgnimes extends React.Component {
     constructor(props) {
@@ -18,7 +33,18 @@ export default class AddEgnimes extends React.Component {
             indices: [],
             info: null,
             image: null,
-            points: null
+            points: null,
+            formErrors: {
+                titre: "",
+                enonce: "",
+                nouvellerep: "",
+                responses: "",
+                nouvelindice: "",
+                indices: "",
+                lat: "",
+                long: "",
+                points: "",
+            }
         };
         this.addResp = [];
         this.Clue1 = null;
@@ -119,6 +145,89 @@ export default class AddEgnimes extends React.Component {
             info: e.target.value
         })
     }
+
+
+    handleSubmit = e => {
+        e.preventDefault();
+        
+        if (formValid(this.state)) {
+            console.log(`
+            titre: ${this.state.titre}
+            enonce: ${this.state.enonce}
+            nouvellerep: ${this.state.nouvellerep}
+            responses: ${this.state.responses}
+            nouvelinces: ${this.state.nouvelindice}
+            indices: ${this.state.indices}
+            lat: ${this.state.lat}
+            long: ${this.state.long}
+            point: ${this.state.points}
+            `);
+        } else {
+            console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
+        }
+    };
+
+    handleChange = e => {
+        e.preventDefault();
+        const { name, value } = e.target;
+        let formErrors = { ...this.state.formErrors };
+
+        console.log(" Titre :", name)
+        console.log(" Value :", value)
+        
+
+        switch (name) {
+            case 'titre':
+                formErrors.titre =
+                    value.length < 3 ? "minimum 3 characaters required" : "";
+                break;
+
+            case 'enonce':
+                formErrors.enonce =
+                value.length < 3 ? "minimum 3 characaters required" : "";
+                break;
+
+            case 'nouvellerep':
+                formErrors.nouvellerep =
+                value.length < 3 ? "minimum 3 characaters required" : "";
+                break;
+
+            case 'nouvelleindice':
+                formErrors.nouvelindice =
+                value.length < 3 ? "minimum 3 characaters required" : "";
+                break;
+
+            case 'lat':
+                formErrors.lat =
+                value.length < 3 ? "minimum 3 characaters required" : "";
+                break;
+
+            case 'long':
+                formErrors.long =
+                value.length < 3 ? "minimum 3 characaters required" : "";
+                break;
+
+            case 'points':
+                formErrors.points =
+                value.length < 3 ? "minimum 3 characaters required" : "";
+                break;
+            case 'responses':
+                formErrors.responses =
+                value.length < 3 ? "minimum 3 characaters required" : "";
+                break;
+
+            case 'indices':
+                formErrors.indices =
+                value.length < 3 ? "minimum 3 characaters required" : "";
+                break;
+            default:
+                break;
+        }
+        this.setState({ formErrors, [name]: value }, () => console.log(this.state));
+    };
+
+
+
     /* Soumissions de l'énigme - Stockage de celle ci en base de donnée */
     submit = () => {
         axios({
@@ -147,6 +256,7 @@ export default class AddEgnimes extends React.Component {
     }
 
     render() {
+        const { formErrors } = this.state;
         console.log(this.state.points)
         return (
             <div>
@@ -166,48 +276,107 @@ export default class AddEgnimes extends React.Component {
 
                 <FormGroup>
                     <Label for="exampleEmail">Titre énigme</Label>
-                    <Input type="titre" name="titre" id="titreennigme" onChange={this.modifyTitle} />
+                    <Input 
+                    className={formErrors.titre > 0 ? "error" : null}
+                    type="titre" 
+                    name="titre" 
+                    id="titreennigme" 
+                    onChange={this.modifyTitle} 
+                    onChange={this.handleChange}
+                    noValidate
+                    />
+                    {formErrors.titre.length > 0 && (
+                        <span className="errorMessage">{formErrors.titre}</span>
+                    )}
+                    
                 </FormGroup>
 
                 <FormGroup>
                     <Label for="exampleText">Énoncé</Label>
-                    <Input type="textarea" name="text" id="exampleText" onChange={this.modifyAnnouncement} />
+                    <Input 
+                    className={formErrors.enonce > 0 ? "error" : null}
+                    type="textarea" 
+                    name="text" 
+                    id="exampleText" 
+                    onChange={this.modifyAnnouncement} 
+                    onChange={this.handleChange}
+                    noValidate
+                    />
+                    <small>*obligatoire</small>
+                    {formErrors.enonce.length > 0 && (
+                        <span className="errorMessage">{formErrors.enonce}</span>
+                    )}
+                    
                 </FormGroup>
 
                 <FormGroup>
                     <Label for="exampleText">Question</Label>
-                    <Input type="textarea" name="text" id="exampleText" onChange={this.modifyQuestion} />
+                    <Input
+                    type="textarea" 
+                    name="text" 
+                    id="exampleText" 
+                    onChange={this.modifyQuestion} 
+                    onChange={this.handleChange}
+                    />
                 </FormGroup>
 
                 <FormGroup>
                     <Label for="exampleText">Réponse</Label>
-                    <Input type="textarea" name="text" id="exampleText" onChange={this.addResponse} />
+                    <Input 
+                    className={formErrors.responses > 0 ? "error" : null}
+                    type="textarea" 
+                    name="text" 
+                    id="exampleText" 
+                    onChange={this.addResponse} 
+                    onChange={this.handleChange}
+                    noValidate
+                    />
+                    {formErrors.responses.length > 0 && (
+                        <span className="errorMessage">{formErrors.responses}</span>
+                    )}
                 </FormGroup>
 
                 <FormGroup>
                     <Label for="exampleText">Points à gagner pour cette énigme</Label>
-                    <Input type="text" name="text" id="exampleText" onChange={this.addPoints} />
+                    <Input 
+                    className={formErrors.points > 0 ? "error" : null}
+                    type="text" 
+                    name="text" 
+                    id="exampleText" 
+                    onChange={this.addPoints} 
+                    onChange={this.handleChange}
+                    noValidate
+                    />
+                    {formErrors.points.length > 0 && (
+                        <span className="errorMessage">{formErrors.points}</span>
+                    )}
                 </FormGroup>
 
                 <FormGroup>
                     <Label for="exampleEmail">Indices</Label>
                     <Input
+                        className={formErrors.indices > 0 ? "error" : null}
                         type="indice"
                         name="indice"
                         placeholder="Indice #1"
                         onChange={this.add1Clue}
+                        onChange={this.handleChange}
                     />
                     <Input
+                        className={formErrors.indices > 0 ? "error" : null}
                         type="indice"
                         name="indice"
                         placeholder="Indice #2"
                         onChange={this.add2Clue}
+                        onChange={this.handleChange}
                     />
                     <Input
+                        className={formErrors.indices > 0 ? "error" : null}
                         type="indice"
                         name="indice"
                         placeholder="Indice #3"
                         onChange={this.add3Clue}
+                        onChange={this.handleChange}
                     />
                 </FormGroup>
 
@@ -215,19 +384,48 @@ export default class AddEgnimes extends React.Component {
                 <h3>Localisation</h3>
                 <FormGroup>
                     <Label for="exampleEmail">Lattitude</Label>
-                    <Input type="titre" name="titre" id="titreennigme" onChange={this.modifyLat} />
+                    <Input 
+                    className={formErrors.lat > 0 ? "error" : null}
+                    type="titre" 
+                    name="titre" 
+                    id="titreennigme" 
+                    onChange={this.modifyLat} 
+                    onChange={this.handleChange}
+                    noValidate
+                    />
+                    {formErrors.lat.length > 0 && (
+                        <span className="errorMessage">{formErrors.lat}</span>
+                    )}
+                    <small>*obligatoire</small>
                 </FormGroup>
                 <FormGroup>
                     <Label for="exampleEmail">Longitude</Label>
-                    <Input type="titre" name="titre" id="titreennigme" onChange={this.modifyLong} />
+                    <Input
+                    className={formErrors.long > 0 ? "error" : null} 
+                    type="titre" 
+                    name="titre" 
+                    id="titreennigme" 
+                    onChange={this.modifyLong} 
+                    onChange={this.handleChange}
+                    noValidate
+                    />
+                    {formErrors.long.length > 0 && (
+                        <span className="errorMessage">{formErrors.long}</span>
+                    )}
+                    <small>*obligatoire</small>
                 </FormGroup>
                 <FormGroup>
                     <Label for="exampleEmail">Règles du lieu</Label>
-                    <Input type="titre" name="titre" id="titreennigme" onChange={this.modifyInfo} />
+                    <Input 
+                    type="titre" 
+                    name="titre" 
+                    id="titreennigme" 
+                    onChange={this.modifyInfo} 
+                    />
                 </FormGroup>
 
                 <Card body>
-                    <Button onClick={this.submit}>Enregistrer les modifications</Button>
+                    <Button onClick={this.submit} noValidate>Enregistrer les modifications</Button>
 
                 </Card>
                 <NavLink to="/Admin/ListEnigmes"><Button>Retour</Button></NavLink>
--- raid-app/src/Components/Admin/AddTeam.jsx
+++ raid-app/src/Components/Admin/AddTeam.jsx
@@ -4,6 +4,29 @@ import axios from 'axios'
 import { NavLink } from 'react-router-dom';
 import generator from 'generate-password'
 
+const emailRegex = RegExp(
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+);
+
+// const telephoneRegex = RegExp(
+//     /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
+// ;
+
+const formValid = ({ formErrors, ...rest }) => {
+    let valid = true;
+
+    // validate form errors being empty
+    Object.values(formErrors).forEach(val => {
+        val.length > 0 && (valid = false);
+    });
+
+    // validate the form was filled out
+    Object.values(rest).forEach(val => {
+        val === null && (valid = false);
+    });
+
+    return valid;
+};
 
 export default class AddTeam extends React.Component {
     constructor(props) {
@@ -12,7 +35,14 @@ export default class AddTeam extends React.Component {
             nom: null,
             participants: [],
             email: null,
-            telephone: null
+            telephone: null,
+            formErrors: {
+                nom: "",
+                participants: "",
+                email: "",
+                telephone: "",
+                titre: "",
+            }
         };
     }
 
@@ -62,6 +92,61 @@ export default class AddTeam extends React.Component {
         })
     }
 
+    handleSubmit = e => {
+        e.preventDefault();
+
+        if (formValid(this.state)) {
+            console.log(`
+            titre: ${this.state.titre}
+            email: ${this.state.email}
+            participants: ${this.state.participants}
+            telephone: ${this.state.telephone}
+            `);
+        } else {
+            console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
+        }
+    };
+
+    handleChange = e => {
+        e.preventDefault();
+        const { name, value } = e.target;
+        let formErrors = { ...this.state.formErrors };
+
+        console.log(" Titre :", name)
+        console.log(" Value :", value)
+        
+
+        switch (name) {
+            case 'titre':
+                formErrors.titre =
+                    value.length < 3 ? "minimum 3 characaters required" : "";
+                break;
+
+            case 'email':
+                formErrors.email = emailRegex.test(value)
+                    ? ""
+                    : "invalid Email address";
+                break;
+
+            case 'telephone':
+                formErrors.telephone =
+                value.length < 10 ? " minimum 10 characteres required" : "";
+                break;
+
+            case 'participants':
+                formErrors.participants =
+                    value.length < 3 ? "minimum 3 characaters required" : "";
+                break;
+            default:
+                break;
+        }
+        this.setState({ formErrors, [name]: value }, () => console.log(this.state));
+    };
+
+
+
+
+
     addParticipants = e => {
         e.preventDefault()
         let participants = this.state.participants.concat([''])
@@ -97,10 +182,11 @@ export default class AddTeam extends React.Component {
             .catch(function (error) {
                 console.log("MERDE", error);
             });
-            window.location.href = 'ListTeam';
+        window.location.href = 'ListTeam';
     }
 
     render() {
+        const { formErrors } = this.state;
         return (
             <div>
 
@@ -108,17 +194,51 @@ export default class AddTeam extends React.Component {
 
                 <FormGroup>
                     <Label for="exampleEmail">Titre de l'équipe</Label>
-                    <Input required type="titre" name="titre" id="titreequipe" onChange={this.modifyNom} />
+                    <Input
+                        className={formErrors.titre > 0 ? "error" : null}
+                        required
+                        type="titre"
+                        name="titre"
+                        id="titreequipe"
+                        noValidate
+                        onChange={this.modifyNom}
+                        onChange={this.handleChange}
+                    />
+                    {formErrors.titre.length > 0 && (
+                        <span className="errorMessage">{formErrors.titre}</span>
+                    )}
                 </FormGroup>
 
                 <FormGroup>
                     <Label for="exampleEmail">E-mail de l'équipe</Label>
-                    <Input type="email" name="email" id="emailequipe" onChange={this.modifyEmail} />
+                    <Input
+                        className={formErrors.email.length > 0 ? "error" : null}
+                        type="email"
+                        name="email"
+                        id="emailequipe"
+                        noValidate
+                        onChange={this.modifyEmail}
+                        onChange={this.handleChange}
+                    />
+                    {formErrors.email.length > 0 && (
+                        <span className="errorMessage">{formErrors.email}</span>
+                    )}
                 </FormGroup>
 
                 <FormGroup>
                     <Label for="exampleEmail">Téléphone de l'équipe</Label>
-                    <Input type="phone" name="phone" id="phoneequipe" onChange={this.modifyTelephone} />
+                    <Input
+                        className={formErrors.telephone.length > 0 ? "error" : null}
+                        type="phone"
+                        name="phone"
+                        id="phoneequipe"
+                        noValidate
+                        onChange={this.modifyTelephone}
+                        onChange={this.handleChange}
+                    />
+                    {formErrors.telephone.length > 0 && (
+                        <span className="errorMessage">{formErrors.telephone}</span>
+                    )}
                 </FormGroup>
 
                 <FormGroup>
@@ -126,11 +246,17 @@ export default class AddTeam extends React.Component {
                     {this.state.participants.map((participants, index) => (
                         <span key={index}>
                             <Input
+                                className={formErrors.participants.length > 0 ? "error" : null}
                                 type="text"
                                 size="2"
                                 onChange={this.handleText(index)}
+                                noValidate
                                 value={participants}
+                                
                             />
+                            {formErrors.participants.length > 0 && (
+                                <span className="errorMessage">{formErrors.participants}</span>
+                            )}
                             <button onClick={this.handleDelete(index)}>supprimer</button>
                             <hr />
                         </span>
@@ -140,7 +266,7 @@ export default class AddTeam extends React.Component {
                 </FormGroup>
 
                 <CardFooter>
-                    <Button onClick={this.submitTeam}>Enregistrer les modifications</Button>
+                    <Button onClick={this.submitTeam} noValidate>Enregistrer les modifications</Button>
                 </CardFooter>
                 <NavLink to="/Admin/ListTeam"><Button>Retour</Button></NavLink>
 
