72c7510e66c51a8699f27bd73d4ba741d843b38f
Jeffrey <je.et.ge@gmail.com>
2019-01-24 11:48:57 +0100

    route admin deco

--- raid-app/src/App.js
+++ raid-app/src/App.js
@@ -31,6 +31,16 @@ export default class App extends Component {
               <Route exact path="/" component={HomePage} />
               <Route path="/EnigmePage/:_id/:id" component={EnigmePage} />
               <Route path="/MapPage/:_id" component={MapPage} />
+            </Switch>
+            :
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+            </Switch>
+          }
+        </BrowserRouter>
+        <BrowserRouter>
+          {window.localStorage.getItem("tokenAdmin") ?
+            <Switch>
               <Route exact path="/Admin" component={Connexion} />
               <Route path="/Admin/AddTeam/:_id" component={AddTeam} />
               <Route path="/Admin/AdminComptes/:_id" component={AdminComptes} />
@@ -48,21 +58,7 @@ export default class App extends Component {
             </Switch>
             :
             <Switch>
-              <Route exact path="/" component={HomePage} />
               <Route exact path="/Admin" component={Connexion} />
-              <Route path="/Admin/AddTeam/:_id" component={AddTeam} />
-              <Route path="/Admin/AdminComptes/:_id" component={AdminComptes} />
-              <Route path="/Admin/Classement/:_id" component={Classement} />
-              <Route path="/Admin/ListEnigmes/:_id" component={ListEnigmes} />
-              <Route path="/Admin/ListAdmin/:_id" component={ListAdmin} />
-              <Route path="/Admin/ListSessionPage/:_id" component={ListSessionPage} />
-              <Route path="/Admin/ListTeam/:_id" component={Listequipes} />
-              <Route path="/Admin/SessionPage/:_id" component={SessionPage} />
-              <Route path="/Admin/AddEgnimes/:_id" component={AddEgnimes} />
-              <Route path='/Admin/Addsession/:_id' component={Addsession} />
-              <Route path='/Admin/QrCodeScan/:_id' component={QrCodeScan} />
-              <Route path="/Admin/enigmes/:_id/:id" component={UnEnigme} />
-              <Route path="/Admin/equipe/:_id/:id" component={uneTeam} />
             </Switch>
           }
         </BrowserRouter>
